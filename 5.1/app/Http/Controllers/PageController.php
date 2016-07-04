87fd6ebdc65c43788cef73d11579bae18bdb3340
Khaled Mahmud Tuhin <tuhiny2k5@gmail.com>
2016-07-04 18:52:25 +0600

    final commit

--- 5.1/app/Http/Controllers/PageController.php
+++ 5.1/app/Http/Controllers/PageController.php
@@ -9,4 +9,19 @@ use App\Http\Requests;
 class PageController extends Controller
 {
     //
+    /**
+     * Bootstrap Form
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function form(){
+        return view('form');
+    }
+
+    /**
+     * Save Data
+     */
+    public function save(Request $request){
+         dd($request->all());
+    }
 }
--- 5.1/app/Http/routes.php
+++ 5.1/app/Http/routes.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@form');
+Route::post('/save', 'PageController@save');
